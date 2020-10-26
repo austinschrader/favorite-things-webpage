4d99a8777ce5a2d53a12dbf4ec22c9bc3f447986
Austin Schrader <austin.jschrader@gmail.com>
2020-10-26 10:50:47 -0700

    Initialize the favorite things website with a working form, and submit button
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	$('form#favoriteThings').submit(function (event) {
+		event.preventDefault();
+
+		let food = $('input#food').val();
+		let superhero = $('input#superhero').val();
+		let place = $('input#place').val();
+		let favoriteThings = [];
+
+		favoriteThings.push(food, superhero, place);
+		console.log(favoriteThings);
+	});
+});
