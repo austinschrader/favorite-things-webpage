ca6a357c5321cc7cc4e53b8fc1a0a6e1e42d77b9
Austin Schrader <austin.jschrader@gmail.com>
2020-10-26 11:09:41 -0700

    Show the 2nd, 1st and 3rd results respectively at the bottom of the page on submit
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,19 @@ $(document).ready(function () {
 		let food = $('input#food').val();
 		let superhero = $('input#superhero').val();
 		let place = $('input#place').val();
+		let country = $('input#country').val();
+		let restaurant = $('input#restaurant').val();
 		let favoriteThings = [];
 
-		favoriteThings.push(food, superhero, place);
-		console.log(favoriteThings);
+		favoriteThings.push(food, superhero, place, country, restaurant);
+
+		let newArray = [];
+		newArray.push(favoriteThings[1], favoriteThings[0], favoriteThings[2]);
+
+		console.log(newArray);
+		$('.favoriteResults').append('<li>' + newArray[0] + '</li>');
+		$('.favoriteResults').append('<li>' + newArray[1] + '</li>');
+		$('.favoriteResults').append('<li>' + newArray[2] + '</li>');
+		$('.favoriteResults').show();
 	});
 });
